9fd7f34661b0082b2ec7b0da50c36e9ec8f0a728
Amit Reuveni <amitreuveni@gmail.com>
2025-11-25 10:35:20 +0200

    Added base for DefaultAbilityBehavior

--- Assets/Scripts/Gameplay/Engine/Abilities/DefaultAbilityBehavior.cs
+++ Assets/Scripts/Gameplay/Engine/Abilities/DefaultAbilityBehavior.cs
@@ -1,7 +1,19 @@
+using System.Collections.Generic;
+
 namespace Gameplay.Engine.Abilities
 {
-    public class DefaultAbilityBehavior
+    public class DefaultAbilityBehavior : IAbilityBehavior
     {
+        public bool IsAbilityAvailable { get; }
         
+        public DefaultAbilityBehavior()
+        {
+            
+        }
+        
+        public IReadOnlyList<AbilityOption> GetAbilityOptions(AbilityContext context)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
